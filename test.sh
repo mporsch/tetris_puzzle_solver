c9e3fcdc519e43d02691a17f5007270e072a2ec8
Marco Porsch <marco.porsch@posteo.de>
2020-05-12 17:47:58 +0200

    add test script with more examples

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+./tetris_puzzle_solver -w 08 -h 8 -T 4 -J 4 -L 1 -O 3 -Z 1 -S 1 -I 2
+./tetris_puzzle_solver -w 08 -h 6 -T 2 -J 3 -L 2 -O 2 -Z 2 -S 0 -I 1
+./tetris_puzzle_solver -w 08 -h 6 -T 2 -J 2 -L 0 -O 2 -Z 3 -S 2 -I 1
+./tetris_puzzle_solver -w 10 -h 4 -T 4 -J 1 -L 0 -O 0 -Z 2 -S 1 -I 2
+./tetris_puzzle_solver -w 10 -h 4 -T 4 -J 0 -L 2 -O 2 -Z 0 -S 1 -I 1
+./tetris_puzzle_solver -w 08 -h 5 -T 4 -J 1 -L 1 -O 0 -Z 2 -S 2 -I 0
+./tetris_puzzle_solver -w 08 -h 5 -T 4 -J 1 -L 2 -O 1 -Z 0 -S 0 -I 2
+./tetris_puzzle_solver -w 08 -h 5 -T 4 -J 1 -L 1 -O 0 -Z 2 -S 2 -I 0
+./tetris_puzzle_solver -w 08 -h 5 -T 2 -J 1 -L 1 -O 2 -Z 1 -S 1 -I 2
